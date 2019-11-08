679c5b056cbf83633adb70325d22dc22f63cc149
Base Camp <basecamp@Bases-MacBook-Pro.local>
2019-11-08 11:23:32 -0600

    Did the whole project without pushing inbetween. My bad.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from app.models import Coffee
+from app.models import Transaction
 
+@admin.register(Coffee)
+class CoffeeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    pass
 # Register your models here.
